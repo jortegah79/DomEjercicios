5d5304166d207dbe4b034e96e61546f038259ad8
jortegah <jortegah79@gmail.com>
2022-08-28 17:48:02 +0200

    Pequeños cambios en ejercicio

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-import {temporizador} from "./temporizador.js";
-import {modoOscuro} from "./darkTheme.js";
+import { temporizador, contador } from "./temporizador.js";
+import { modoOscuro } from "./darkTheme.js";
 import responsiveMedia from "./responsives.js";
-import {nueva_ventana,cerrar} from "./responsiveTester.js";
+import { nueva_ventana, cerrar } from "./responsiveTester.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import connStatus from "./connectionStatus.js";
-import {muestraWebcam,detieneWebcam} from "./webcam.js";
+import { muestraWebcam, detieneWebcam } from "./webcam.js";
 import gioLocalizacion from "./glocalizacion.js";
 import handleText from "./buscaElementos.js";
 import concursoLng from "./concurso.js";
@@ -13,8 +13,8 @@ import controlSlider from "./sliders.js";
 /***********************Botón 1 ----Menú*************************************** */
 
 const d = document,
-w=window,
-  $menu = d.getElementById("menu"),  
+  w = window,
+  $menu = d.getElementById("menu"),
   $boton = d.getElementById("boton");
 
 $boton.addEventListener("click", () => {
@@ -24,24 +24,24 @@ $boton.addEventListener("click", () => {
 
 /**************************Botón 2 -----Control scroll************************************************************** */
 
-const $boton2=d.getElementById("boton2");
+const $boton2 = d.getElementById("boton2");
 
-window.addEventListener("scroll",()=>{
-  if(window.scrollY>=800){
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 800) {
     $boton2.classList.remove("oculto");
-  
-    
-  }else{
+
+
+  } else {
     $boton2.classList.add("oculto");
   }
 })
-$boton2.addEventListener("click",()=>{
-    window.scrollTo(0,0);
+$boton2.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 })
 
 
 
-/**********************************************************AUDIOS Y RELOJ*********************** */ 
+/**********************************************************AUDIOS Y RELOJ*********************** */
 
 const $audio = d.getElementById("beep"),
   $start = d.getElementById("Start"),
@@ -55,13 +55,10 @@ let reloj = "";
 $start.addEventListener("click", () => {
   reloj = setInterval(() => {
     let fecha = new Date();
-    $time.innerHTML = `${
-      fecha.getHours() < 10 ? "0" + fecha.getHours() : fecha.getHours()
-    }:${
-      fecha.getMinutes() < 10 ? "0" + fecha.getMinutes() : fecha.getMinutes()
-    }:${
-      fecha.getSeconds() < 10 ? "0" + fecha.getSeconds() : fecha.getSeconds()
-    }`;
+    $time.innerHTML = `${fecha.getHours() < 10 ? "0" + fecha.getHours() : fecha.getHours()
+      }:${fecha.getMinutes() < 10 ? "0" + fecha.getMinutes() : fecha.getMinutes()
+      }:${fecha.getSeconds() < 10 ? "0" + fecha.getSeconds() : fecha.getSeconds()
+      }`;
     $start.setAttribute("disabled", "true");
     $start.classList.add("activo");
   }, 1000);
@@ -71,7 +68,7 @@ $stop.addEventListener("click", () => {
   clearInterval(reloj);
   $start.removeAttribute("disabled");
   $start.classList.remove("activo");
-  $time.innerHTML=`Stop`;
+  $time.innerHTML = `Stop`;
 });
 
 $startAlarm.addEventListener("click", () => {
@@ -85,11 +82,11 @@ $stopAlarm.addEventListener("click", () => {
   $startAlarm.classList.remove("activo");
 });
 /**************************fin reloj*********************** */
-d.addEventListener("DOMContentLoaded",()=>{
-  
+d.addEventListener("DOMContentLoaded", () => {
+
 })
 const $ball = d.getElementById("ball"),
-$pizarra=d.getElementById("pizarra"),
+  $pizarra = d.getElementById("pizarra"),
   MAX_WIDTH = $pizarra.getBoundingClientRect().width - $ball.getBoundingClientRect().width,
   MIN_WIDTH = 0,
   MAX_HEIGHT = $pizarra.getBoundingClientRect().height - $ball.getBoundingClientRect().height,
@@ -104,7 +101,7 @@ d.addEventListener("keydown", (e) => {
     if (posx < MAX_WIDTH) {
       posx += 10;
       $ball.style.setProperty("transform", `translate(${posx}px,${posy}px)`);
-    }else{
+    } else {
       alert($ball.style.getProperty("width"));
     }
   }
@@ -141,60 +138,74 @@ d.addEventListener("keydown", (e) => {
 });
 /***********************cuenta atras y reposonsible responsive************************************* */
 
-d.addEventListener("DOMContentLoaded",()=>{
-  temporizador("countdown","txt-countdown",'2023-1-1 0:0',"El programador ninja les desea ¡¡¡FELIZ AÑO NUEVO!!!");
-  responsiveMedia("youtube","(min-width:1024px)",`<a href="https://www.youtube.com/embed/5k2DWMRTXMM" noopener noreferrer target="_blank">Ir al video</a>`,`<iframe width="600" height="350" src="https://www.youtube.com/embed/5k2DWMRTXMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
-  responsiveMedia("map","(min-width:1024px)",`<a href="https://goo.gl/maps/abYCiW65uprrBSLGA" noopener noreferrer target="_blank">Ir al mapa</a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.6711244945695!2d2.017773615683429!3d41.31601760837904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49cf18b64c017%3A0x6fc52cfd5c30c223!2sAyuntamiento%20de%20Viladecans!5e0!3m2!1ses!2ses!4v1655286803598!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
- gioLocalizacion('geoResult');
- concursoLng("btn_concurso",".opcion","resConcurso");
- controlSlider();
+d.addEventListener("DOMContentLoaded", () => {
+  temporizador("countdown", "txt-countdown", '2023-1-1 0:0', "El programador ninja les desea ¡¡¡FELIZ AÑO NUEVO!!!");
+  responsiveMedia("youtube", "(min-width:1024px)", `<a href="https://www.youtube.com/embed/5k2DWMRTXMM" noopener noreferrer target="_blank">Ir al video</a>`, `<iframe width="600" height="350" src="https://www.youtube.com/embed/5k2DWMRTXMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+  responsiveMedia("map", "(min-width:1024px)", `<a href="https://goo.gl/maps/abYCiW65uprrBSLGA" noopener noreferrer target="_blank">Ir al mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.6711244945695!2d2.017773615683429!3d41.31601760837904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49cf18b64c017%3A0x6fc52cfd5c30c223!2sAyuntamiento%20de%20Viladecans!5e0!3m2!1ses!2ses!4v1655286803598!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
+  gioLocalizacion('geoResult');
+  concursoLng("btn_concurso", ".opcion", "resConcurso");
+  controlSlider();
 
-}) 
+})
 /******************modo oscuro************************************************** */
 
- modoOscuro(".dark-theme-btn","dark-mode");
+modoOscuro(".dark-theme-btn", "dark-mode");
 
- /**************************Responsive Tester************************************************************************* */
-
-
- const $uri=d.getElementById("uri-web"),
- $ancho=d.getElementById("res-x"),
- $alto=d.getElementById("res-y");
+/**************************Responsive Tester************************************************************************* */
 
 
- d.addEventListener("click",(e)=>{
- 
+const $uri = d.getElementById("uri-web"),
+  $ancho = d.getElementById("res-x"),
+  $alto = d.getElementById("res-y");
+
+
+d.addEventListener("click", (e) => {
+ let detencion;
   e.preventDefault;
-   if(e.target.id==="btn-probe"){
+  if (e.target.id === "btn-probe") {
     console.log(`uri:${$uri.value}<br>
     ancho: ${$ancho.value}<br>
     alto:${$alto.value}`);
-    nueva_ventana(`${$uri.value}`,`${$ancho.value}`,`${$alto.value}`);
-   }
-   if(e.target.id==="btn-close"){
-     cerrar();
-   }
- })
-
- /*****************************DETECCION DISPOSITIVOS********** */
- d.addEventListener("DOMContentLoaded",userDeviceInfo("userA"));
-
-
- /****************************DETECCIÓN DE LA CONEXIÓN************************************************************* */
-  connStatus("estado");
-  /*****************************MOSTRAR LA WEBCAM********************************************** */
-  
-  d.addEventListener("click",(e)=>{
-    if(e.target.id==="play"){
-      muestraWebcam("webcamVideo");
-    }
-    if(e.target.id==="stop"){
-      detieneWebcam("webcamVideo");
-    }
+    nueva_ventana(`${$uri.value}`, `${$ancho.value}`, `${$alto.value}`);
+  }
+  if (e.target.id === "btn-close") {
+    cerrar();
+  }
+  if (e.target.id === "envio-fecha-inicial") {
+    let fecha = d.getElementById("fecha-inicial").value;
     
-  })
- /***************************************BÚSQUEDAS RÁPIDAS********************************************************************* */
-    handleText(".card");
- 
+    if (fecha) {
+     detencion= contador("msg-ini-date", fecha,"div-cont1","div-cont2");
+    } else {
+      d.getElementById('msg-ini-date').innerHTML = `No has introducido una fecha`;
+    }
+  }
+   if(e.target.id==="detener-fecha-inicial"){
+     clearInterval(detencion);
+     d.getElementById("div-cont1").style.display="block";
+     d.getElementById("div-cont2").style.display="none";
+   }
+})
 
- 
+/*****************************DETECCION DISPOSITIVOS********** */
+d.addEventListener("DOMContentLoaded", userDeviceInfo("userA"));
+
+
+/****************************DETECCIÓN DE LA CONEXIÓN************************************************************* */
+connStatus("estado");
+/*****************************MOSTRAR LA WEBCAM********************************************** */
+
+d.addEventListener("click", (e) => {
+  if (e.target.id === "play") {
+    muestraWebcam("webcamVideo");
+  }
+  if (e.target.id === "stop") {
+    detieneWebcam("webcamVideo");
+  }
+
+})
+/***************************************BÚSQUEDAS RÁPIDAS********************************************************************* */
+handleText(".card");
+
+
+
--- js/temporizador.js
+++ js/temporizador.js
@@ -1,33 +1,61 @@
-const d=document;
+const d = document;
+const DIA = 60 * 60 * 24;
+const HORA = 60 * 60;
+const MINUTO = 60;
+const SEGUNDO = 1000;
 
-export let temporizador=((contenedor,muestraTxt,fechaFinal,mensaje)=>{
-//constantes
-let fechaFin=new Date(fechaFinal).getTime();
-let txtResActual=d.getElementById(contenedor);
-let txtMensaje=d.getElementById(muestraTxt);
-let tiActual;
-const DIA=60*60*24;
-const HORA=60*60;
-const MINUTO=60;
-const SEGUNDO=1000;
-setInterval(()=>{
-    let hoy=new Date().getTime();
-      tiActual=tiempo(fechaFin-hoy,SEGUNDO);
-      let dias=tiempo(tiActual,DIA);
-      let horas=tiempo(resto(tiActual,DIA),HORA);
-      let minutos=tiempo(resto(tiActual,HORA),MINUTO);
-      let segundos=tiempo(resto(tiActual,MINUTO),1);
-      txtResActual.innerHTML=`${dias} dias ${horas} horas ${minutos} minutos y ${segundos} segundos <BR>para ${new Date(fechaFinal).toLocaleDateString()}  ${new Date(fechaFinal).toLocaleTimeString()} `;
-      if(dias===0 &&horas===0 && minutos===0 && segundos===0){
-        txtMensaje.innerHTML=`${mensaje}`;
-      }      
-},1000);
+export let temporizador = ((contenedor, muestraTxt, fechaFinal, mensaje) => {
+  //constantes
+  let fechaFin = new Date(fechaFinal).getTime();
+  let txtResActual = d.getElementById(contenedor);
+  let txtMensaje = d.getElementById(muestraTxt);
+  let tiActual;
+
+  setInterval(() => {
+   
+    let hoy = new Date().getTime();
+    tiActual = tiempo(fechaFin - hoy, SEGUNDO);
+    let dias = tiempo(tiActual, DIA);
+    let horas = tiempo(resto(tiActual, DIA), HORA);
+    let minutos = tiempo(resto(tiActual, HORA), MINUTO);
+    let segundos = tiempo(resto(tiActual, MINUTO), 1);
+    txtResActual.innerHTML = `${dias} dias ${horas} horas ${minutos} minutos y ${segundos} segundos <BR>para ${new Date(fechaFinal).toLocaleDateString()}  ${new Date(fechaFinal).toLocaleTimeString()} `;
+    if (dias === 0 && horas === 0 && minutos === 0 && segundos === 0) {
+      txtMensaje.innerHTML = `${mensaje}`;
+    }
+  }, 1000);
+
 });
 
-let tiempo= ((valorTiempo,divisor)=>{
-          if(valorTiempo===0)return 0;
-  return Math.floor(valorTiempo/divisor);
+export let contador = ((muestra_dias, fechaInicial,caja,button_del) => {
+  
+  d.getElementById(caja).style.display="none";
+  d.getElementById(button_del).style.display="block";
+   let fechaInicio = new Date(fechaInicial).getTime();
+  let txtActual = d.getElementById(muestra_dias);
+  let tiActual;
+   let terminar;
+   terminar= setInterval(() => {
+    
+    let hoy = new Date().getTime();
+    tiActual = tiempo(hoy - fechaInicio, SEGUNDO);
+    let dias = tiempo(tiActual, DIA);
+    txtActual.innerHTML = `${dias} dias <BR>desde  ${new Date(fechaInicial).toLocaleDateString()} hasta hoy.`;
+    console.log("actua");
+    d.addEventListener("click",(e)=>{
+      if(e.target.id==="detener-fecha-inicial"){
+        clearInterval(terminar);
+      }
+      });
+  }, 1000);
+
 });
-let resto=((valorTiempo,divisor)=>{
-  return valorTiempo%divisor;  
+
+
+let tiempo = ((valorTiempo, divisor) => {
+  if (valorTiempo === 0) return 0;
+  return Math.floor(valorTiempo / divisor);
+});
+let resto = ((valorTiempo, divisor) => {
+  return valorTiempo % divisor;
 });
